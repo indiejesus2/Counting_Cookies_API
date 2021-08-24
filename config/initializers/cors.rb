db7ae517bde261e9b5cdc9c1e54cd46da51c1396
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2021-08-24 10:37:23 -0400

    WIP: Adding heroku origin to cors.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'localhost:3001'
+      origins 'localhost:3001', 'counting-cookies.herokuapp.com'
       resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options], credentials: true
     end
   end
