91b4fe2309b673a0e40f378d0a43ef8cd0b3bcd3
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2021-08-30 23:27:48 -0400

    WIP: Had to update CORS to accept new local host address.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'localhost:3001', 'counting-cookies.herokuapp.com'
+      origins 'localhost:3000', 'counting-cookies.herokuapp.com'
       resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options], credentials: true
     end
   end
